1cf40d65638b108dd09ae6ebe890ea7e7cdf675d
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-05-15 08:41:13 +0545

    variables

--- first.go
+++ first.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go!")
+	// var city = "Kathmandu"
+	var name string
+	const human = "amu"
+	name = "shakti"
+	const num1 = 50
+	var num float32
+	num = 100
+	var num2 = 60
+	var total float32
+	total = num + num1
+	fmt.Println("the sum is", "\t", total, "\t", "name is", "\t", name, "\t", "second number is", "\t", num2)
 }
