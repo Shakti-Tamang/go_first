f0dee0649578e872cfa312e4870c42241b12fe62
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-05-15 08:51:14 +0545

    prac

--- first.go
+++ first.go
@@ -2,16 +2,51 @@ package main
 
 import "fmt"
 
+var (
+	nu1    int
+	nu2    float32
+	nu3    float64
+	n      string
+	verify bool
+)
+
+const (
+	role = "admin"
+
+	age = 20
+
+	grade = 20.5
+
+	notVerify = false
+)
+
 func main() {
 	// var city = "Kathmandu"
 	var name string
+
+	message := "akjsb"
+
+	n1 := 4
+
+	verified := false
+
 	const human = "amu"
+
 	name = "shakti"
+
 	const num1 = 50
+
 	var num float32
+
 	num = 100
+
 	var num2 = 60
+
 	var total float32
 	total = num + num1
 	fmt.Println("the sum is", "\t", total, "\t", "name is", "\t", name, "\t", "second number is", "\t", num2)
+	fmt.Println("message", "\t", message)
+	fmt.Println("numbers", "\t", n1)
+	fmt.Println(verified)
+
 }
