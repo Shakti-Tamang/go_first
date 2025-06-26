5d58b307e0ab296475ecac58e886669eae34c23c
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-06-26 14:14:51 +0545

    feat:stream api

---
+++ filter.java
@@ -0,0 +1,13 @@
+public class filter {
+
+int age;String name;
+String address;
+double slary;
+String uniqueIdentity;
+public filter(int age,String name,String address,String uniqueIdentity){
+    this.age=age;
+    this.name=name;
+    this.address=address;
+    this.uniqueIdentity=uniqueIdentity;
+}
+}
---
+++ mapping.java
@@ -0,0 +1,18 @@
+public class mapping {
+    String name,address;
+    public void setName(String name){
+this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+        public void setAddress(String address){
+this.address=address;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+}
